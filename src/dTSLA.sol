74e04214864e3e1aa011c040a40ced884c07378f
EggsyOnCode <farzansaqib2003@gmail.com>
2024-07-14 21:48:13 +0500

    dTSLA contract all func done

--- src/dTSLA.sol
+++ src/dTSLA.sol
@@ -5,14 +5,22 @@ import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/Confir
 import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/FunctionsClient.sol";
 import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/libraries/FunctionsRequest.sol";
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract dTSLA is ConfirmedOwner, FunctionsClient {
+contract dTSLA is ConfirmedOwner, FunctionsClient, ERC20 {
     using FunctionsRequest for FunctionsRequest.Request;
+    using Strings for uint256;
 
     // Errors
     error dTSLA__NotEnoughCollateral();
+    error dTSLA__BalanceDoesntMeetMINRequirement();
+    error dTSLA__WithdrawalFailed();
 
     // Events
+    event dTSLA__MintedTokens(address indexed requester, uint256 tokens);
+    event dTSLA__RedeemedTokens(address indexed requester, uint256 usdc);
+    event dTSLA__Withdrawal(address indexed requester, uint256 usdc);
 
     enum MintOrRedeem {
         mint,
@@ -27,32 +35,44 @@ contract dTSLA is ConfirmedOwner, FunctionsClient {
     }
 
     string i_mintSourceCode;
+    string i_redeemSourceCode;
     address private immutable i_priceFeed;
+    address private immutable i_usdcUsdFeed;
 
     uint256 private s_portfolioBalance;
     uint64 private immutable i_subId;
     bytes32 private immutable i_donId;
     mapping(bytes32 requestId => dTSLARequest request) private s_reqIdtoReq;
+    mapping(address user => uint256) private s_userRedemptionWithdrawalQueue;
 
     //CONSTANTS
     // address public SEPOLIA_FUNCTIONS_ROUTER = 0xb83E47C2bC239B3bf370bc41e1459A34b41238D0;
     // bytes32 constant DON_ID = hex"66756e2d657468657265756d2d7365706f6c69612d3100000000000000000000";
     // address public LINK_USDT_PRICE_FEED = 0xc59E3633BAAC79493d908e63626716e204A45EdF;
+    address constant USDC_CONTRACT_SEPOLIA = 0xf08A50178dfcDe18524640EA6618a1f965821715;
     uint32 constant GAS_LIMIT = 300_000;
+    uint256 constant MINIMUM_REDEMPTION = 100;
     uint256 constant PRECISION = 1e18;
     uint256 constant COLLATERAL_RATIO = 200;
     // our protocol is 200% overcollateralized; that means that only if we have 200 USD in collateral (TSLA stocks) can we  mint 100 USD worth of dTSLA
     uint256 constant COLLATERAL_PRECISION = 100;
     uint256 constant ADDITIONAL_FEED_PRECISION = 1e10;
 
-    constructor(address _router, string memory _mintSourceCode, uint64 _subId, bytes32 _donId, address _priceFeed)
-        ConfirmedOwner(msg.sender)
-        FunctionsClient(_router)
-    {
+    constructor(
+        address _router,
+        string memory _mintSourceCode,
+        string memory _redeemSourceCode,
+        uint64 _subId,
+        bytes32 _donId,
+        address _priceFeed,
+        address _usdcUsdtPriceFeed
+    ) ConfirmedOwner(msg.sender) FunctionsClient(_router) ERC20("dTSLA", "dTSLA") {
         i_mintSourceCode = _mintSourceCode;
+        i_redeemSourceCode = _redeemSourceCode;
         i_subId = _subId;
         i_donId = _donId;
         i_priceFeed = _priceFeed;
+        i_usdcUsdFeed = _usdcUsdtPriceFeed;
     }
 
     /// @notice send HTTP req to chianlink along with func that we wish to call to
@@ -60,9 +80,9 @@ contract dTSLA is ConfirmedOwner, FunctionsClient {
     /// 2. calculate how much dTSLA tokens we can mint
     /// 3. mint the tokens
     /// @param _amt amount of dTSLA tokens the user wishes to mint
-    function sendMintRequest(uint256 _amt, string calldata _req) public onlyOwner returns (bytes32) {
+    function sendMintRequest(uint256 _amt) public onlyOwner returns (bytes32) {
         FunctionsRequest.Request memory req;
-        req.initializeRequestForInlineJavaScript(_req);
+        req.initializeRequestForInlineJavaScript(i_mintSourceCode);
         bytes32 reqID = _sendRequest(req.encodeCBOR(), i_subId, GAS_LIMIT, i_donId);
         s_reqIdtoReq[reqID] = dTSLARequest(msg.sender, MintOrRedeem.mint, _amt);
         return reqID;
@@ -79,6 +99,7 @@ contract dTSLA is ConfirmedOwner, FunctionsClient {
         }
         if (toBeMinted != 0) {
             _mint(s_reqIdtoReq[requestId].requester, toBeMinted);
+            emit dTSLA__MintedTokens(s_reqIdtoReq[requestId].requester, toBeMinted);
         }
     }
 
@@ -87,12 +108,57 @@ contract dTSLA is ConfirmedOwner, FunctionsClient {
     /// 1. sell current TSLA stocks in account
     /// 2. buy equivalent USDC in brokerage
     /// 3. send USDC to contract
-    function sendRedeemRequest() public {}
+    /// @param _amtdTsla: dTSLA tokens to be redeemed
+    function sendRedeemRequest(uint256 _amtdTsla) public {
+        uint256 usdcValueofTokens = getUSDCValueofUSDT(getUsdtValueofTSLA(_amtdTsla));
 
-    function _redeemFullFillRequest() internal {}
+        if (usdcValueofTokens < MINIMUM_REDEMPTION) {
+            revert dTSLA__BalanceDoesntMeetMINRequirement();
+        }
+
+        string[] memory args = new string[](2);
+        args[0] = _amtdTsla.toString();
+        args[1] = usdcValueofTokens.toString();
+
+        // call to brokerage to sell TSLA stocks and buy USDC
+        FunctionsRequest.Request memory req;
+        req.initializeRequestForInlineJavaScript(i_redeemSourceCode);
+        req.args = args;
+        bytes32 reqID = _sendRequest(req.encodeCBOR(), i_subId, GAS_LIMIT, i_donId);
+        s_reqIdtoReq[reqID] = dTSLARequest(msg.sender, MintOrRedeem.redeem, _amtdTsla);
+
+        // burn the dTSLA tokens from the msg.sender's account
+        _burn(msg.sender, _amtdTsla);
+    }
+
+    function _redeemFullFillRequest(bytes32 requestId, bytes memory response) internal {
+        uint256 usdcFromRes = uint256(bytes32(response));
+        dTSLARequest memory req = s_reqIdtoReq[requestId];
+
+        // there was some error from the backedn and we couldnt get the USDC value
+        if (usdcFromRes == 0) {
+            _mint(req.requester, req.tokens);
+            return;
+        }
+
+        s_userRedemptionWithdrawalQueue[req.requester] += usdcFromRes;
+        emit dTSLA__RedeemedTokens(req.requester, usdcFromRes);
+    }
+
+    function withdrawRedemptionTokens() public {
+        uint256 usdc = s_userRedemptionWithdrawalQueue[msg.sender];
+        s_userRedemptionWithdrawalQueue[msg.sender] = 0;
+        // transfer the USDC to the user
+        bool suc = ERC20(USDC_CONTRACT_SEPOLIA).transfer(msg.sender, usdc);
+        if (!suc) {
+            revert dTSLA__WithdrawalFailed();
+        }
+
+        emit dTSLA__Withdrawal(msg.sender, usdc);
+    }
 
     /// @dev Internal because Chainlink's gonan call handleOracleFullfillment externally and handleOracle will call fullReq intenrally
-    function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory err) internal override {
+    function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory /*err*/ ) internal override {
         if (s_reqIdtoReq[requestId].mintOrRedeem == MintOrRedeem.mint) {
             _mintFullfillRequest(requestId, response);
         } else {
@@ -112,8 +178,24 @@ contract dTSLA is ConfirmedOwner, FunctionsClient {
 
     /// @dev returns the price of TSLA stock in USD
     function getTSLAPrice() internal view returns (uint256) {
-        AggregatorV3Interface memory priceFeed = AggregatorV3Interface(i_priceFeed);
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(i_priceFeed);
         (, int256 price,,,) = priceFeed.latestRoundData();
         return uint256(price) * ADDITIONAL_FEED_PRECISION; // so that we have 18 decimals in the price
+    }
+
+    function getUsdcPrice() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(i_usdcUsdFeed);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        return uint256(price) * ADDITIONAL_FEED_PRECISION;
+    }
+
+    /// @param _tokens: amount of dTSLA tokens
+    function getUsdtValueofTSLA(uint256 _tokens) internal view returns (uint256) {
+        return (_tokens * getTSLAPrice()) / PRECISION;
+    }
+
+    /// @param _usdtTokens: amount of USDT tokens
+    function getUSDCValueofUSDT(uint256 _usdtTokens) internal view returns (uint256) {
+        return (_usdtTokens * getUsdcPrice()) / PRECISION;
     }
 }
